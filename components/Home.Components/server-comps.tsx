d9703008d04ba668dd2ccba3228c4e5febd3ab41
Daniel Igwe <daniel.igwe000@gmail.com>
2025-09-03 00:44:42 +0100

    added new features

--- components/Home.Components/server-comps.tsx
+++ components/Home.Components/server-comps.tsx
@@ -1,6 +1,3 @@
-//import { ProjectCardProps } from "@/types/project";
-import { FeaturedProjects } from "@/app/(data)/static";
-
 export function HeroSection() {
     return (
         <section className="hero flex flex-col md:flex-row-reverse justify-between items-center gap-20 px-4 py-10 md:px-20 md:py-20 h-[100%]">
@@ -26,39 +23,3 @@ export function HeroSection() {
         </section>
     );
 }
-
-
-export function ProjectSection() {
-    return (
-        <section id="projects" className="px-4 md:px-30 bg-black py-3 pt-10">
-            <div className="flex flex-col w-full">
-                <h2 className="text-white text-2xl md:text-4xl font-bold">My Projects</h2>
-                
-            </div>
-            {/* <div className="bg-black grid grid-cols-1 md:grid-cols-2 gap-6 mt-10 mb-20">
-                {FeaturedProjects.map((project) => (
-                    <ProjectCard key={project.title} {...project} />
-                ))}
-            </div> */}
-        </section>
-    );
-}
-
-
-//function ProjectCard({ title, description, image, link }: ProjectCardProps) {
-//    return (
-//        <div className="bg-black shadow-lg p-6 flex flex-col items-center text-center border border-[#C1F025] rounded-3xl">
-//            <h1 className="text-2xl text-white font-bold mb-2">{title}</h1>
-//            <div className="w-full h-[400px] rounded-md mb-4 overflow-hidden flex items-center justify-center">
-//                <img 
-//                    src={image}
-//                    alt={title}
-//                    className="w-full h-full object-contain transition-transform duration-300 ease-in-out hover:scale-110"
-//                />
-//            </div>
-//            <h3 className="text-xl font-bold mb-2">{title}</h3>
-//            <p className="text-white mb-4">{description}</p>
-//            <a href={link} className="mt-6 px-6 py-3 bg-[#C1F025] text-black font-bold rounded shadow-lg hover:border-[#C1F025] hover:border-1 hover:bg-black hover:text-[#C1F025] shadow-[3px_3px_0_0_rgba(234,179,8,0.7)] transition duration-300">Learn more</a>
-//        </div>
-//    );
-//}
